e500f00392abe95ce5263646e6cb6f8a77939ae5
Baejinho03 <wlsghz69@gmail.com>
2025-05-08 21:05:47 +0900

    Add files via upload

---
+++ [2-1]재귀함수의_꽃 .cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void a(int n) {
+
+    if (n == 0) {
+        return;
+    }
+    
+    cout << n << " ";
+    a(n-1);
+    cout << n << " ";
+
+}
+
+int main()
+{
+    cin >> n;
+
+    a(n);
+
+    return 0;
+}
