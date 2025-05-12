ba9cc27fa1e4f074eda8c653d61facc8f506d17c
Baejinho03 <wlsghz69@gmail.com>
2025-05-12 21:03:10 +0900

    Add files via upload

---
+++ [4-1]정수_명령_처리_5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n;
+	vector<int> v;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		string command;
+		cin >> command;
+
+		if (command == "push_back") {
+			int num;
+			cin >> num;
+			v.push_back(num);
+		}
+
+		else if (command == "pop_back") {
+			v.pop_back();
+		}
+
+		else if (command == "size") {
+			cout << v.size() << endl;
+		}
+		
+		else {
+			int index;
+			cin >> index;
+			cout << v[index - 1] << endl;
+		}	
+	}
+	return 0;
+}
