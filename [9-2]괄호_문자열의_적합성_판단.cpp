ee9501d82424214c286ccc0b7ee19d4a80ffafed
Baejinho03 <wlsghz69@gmail.com>
2025-06-05 23:21:58 +0900

    Add files via upload

---
+++ [9-2]괄호_문자열의_적합성_판단.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+string str;
+stack<char> s;
+
+int main() {
+
+    cin >> str;
+
+    for (int i = 0; i < (int)str.size(); i++) {
+        if (str[i] == '(')
+            s.push('(');
+        else {
+            if (s.empty()) {
+                cout << "No";
+                return 0;
+            }
+            s.pop();
+        }
+    }
+
+    if (s.empty())
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
